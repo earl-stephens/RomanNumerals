6f5d1151d483f80e744fb9ffa2d2a801bab6b436
earl-stephens <stephensearl@hotmail.com>
2023-05-12 21:45:58 -0600

    Test and code for two of the same input characters

--- src/main/java/romanNumerals/Solution.java
+++ src/main/java/romanNumerals/Solution.java
@@ -3,12 +3,15 @@ package romanNumerals;
 public class Solution {
 
 	public int romanToInt(String input) {
+		String[] inputArray = inputToArray(input);
 		int output = 0;
-		String formattedInput = input.toUpperCase();
-		try {
-			output = Numeral.valueOf(formattedInput).getIntValue();
-		} catch(IllegalArgumentException e) {
-			throw new IllegalArgumentException("That is not a valid input value.", e);
+		
+		for(String letter : inputArray) {
+			try {
+				output = output + Numeral.valueOf(letter.toUpperCase()).getIntValue();
+			} catch(IllegalArgumentException e) {
+				throw new IllegalArgumentException("That is not a valid input value.", e);
+			}
 		}
 		return output;
 	}
